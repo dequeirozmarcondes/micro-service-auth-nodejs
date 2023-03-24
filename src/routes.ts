b59fe1b3bed1f59c1c2d7b7c342a9b9ee61817a7
dequeirozmarcondes <dequeirozmarcondes@gmail.com>
2023-03-24 19:56:23 -0400

    feature - tratamento de erros

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  return res.json({ domino: "www.renatodequeirozmarcondes.site" });
+  throw new Error("Erro ao fazer essa requisiçãop.");
 });
 
 export { router };

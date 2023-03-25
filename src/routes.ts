fd6abd2c59efa0c921b2a5e3477f6afcda28a01f
dequeirozmarcondes <dequeirozmarcondes@gmail.com>
2023-03-24 20:10:12 -0400

    feature - configurando o cors

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  throw new Error("Erro ao fazer essa requisiçãop.");
+  return res.json({ name: "Renato" });
 });
 
 export { router };

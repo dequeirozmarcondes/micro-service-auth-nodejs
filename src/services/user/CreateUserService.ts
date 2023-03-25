cdb79bcf64e4dee5649b5d0154621764735a4f96
dequeirozmarcondes <dequeirozmarcondes@gmail.com>
2023-03-24 22:17:16 -0400

    feature - criptografando password

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,6 @@
+import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
+
 interface UserRequest {
   name: string;
   lastname: string;
@@ -7,7 +10,32 @@ interface UserRequest {
 
 class CreateUserService {
   async execute({ name, lastname, email, password }: UserRequest) {
-    return { name: name };
+    if (!name || !lastname || !email || !password) {
+      throw new Error("Preencha todos os dados!");
+    }
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (userAlreadyExists) {
+      throw new Error("E-mail já cadastrado!");
+    }
+    const passwordHash = await hash(password, 8);
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        lastname: lastname,
+        email: email,
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        name: true,
+        lastname: true,
+      },
+    });
+    return user;
   }
 }
 
